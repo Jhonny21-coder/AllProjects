1e2d933c7d539f92639ee5b1c305ad60ffd751cd
Jhon Lee Marahay <shanemarahay@gmail.com>
2024-02-18 00:32:12 +0800

    Initial commit

---
+++ ShoppingCartProject/src/main/java/com/urunov/constants/PusherConstants.java
@@ -0,0 +1,23 @@
+package com.urunov.constants;
+
+/**
+ * User: hamdamboy
+ * Project: card
+ * Github: @urunov
+ */
+public interface PusherConstants {
+
+    /** Pusher App ID */
+    String PUSHER_APP_ID = "1757883";
+
+    /** Pusher App Key */
+    String PUSHER_APP_KEY = "c0f99d5a6e17997ac855";
+
+    /** Pusher App Secret */
+    String PUSHER_APP_SECRET = "a305d518afbca17036a9";
+
+    String PUSHER_CLUSTER_KEY = "ap1";
+
+    /** Pusher channel name where the events will be published */
+    String CHANNEL_NAME = "card-application";
+}
